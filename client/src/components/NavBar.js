c006fc1235fa5e50dfae4e686bf225cfc78caff2
artmaka <artartmako@gmail.com>
2024-04-21 23:18:21 +0500

    фронт-бар клиента

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <div className="navbar container">
+            <a href="#!" className="logo">Let's c<span>oo</span>k!</a>
+            <div className="nav-links">
+                <a href="#!" className="active">Главная</a>
+                <a href="#!">Категории</a>
+                <a href="#!">Профиль</a>
+                <a href="#!">Настройки</a>
+            </div>
+        </div>
+    )
+}
