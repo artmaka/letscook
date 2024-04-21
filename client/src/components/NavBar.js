4ca3f25e617742315d2c56cfba1ce53ceddfa731
artmaka <artartmako@gmail.com>
2024-04-22 02:50:02 +0500

    готовый side-bar, перед проработкой мейн страницы

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,53 @@
+import { useState } from "react"
+import Sidebar from "./Sidebar"
+
+import {faHome, faUser, faList, faCog } from "@fortawesome/free-solid-svg-icons"
+
 export default function Navbar() {
+    const [showSidebar, setShowSidebar] = useState(false)
+    const links = [
+        {
+            name: "Главная",
+            path: "/",
+            icon: faHome
+        },
+        {
+            name: "Категории",
+            path: "/categories",
+            icon: faList
+        },
+        {
+            name: "Профиль",
+            path: "/profile",
+            icon: faUser
+        },
+        {
+            name: "Настройки",
+            path: "/settings",
+            icon: faCog
+        },
+    ]
+
+    function closeSidebar() {
+        setShowSidebar(false);
+    }
+
     return (
+        <>
         <div className="navbar container">
             <a href="#!" className="logo">Let's c<span>oo</span>k!</a>
             <div className="nav-links">
-                <a href="#!" className="active">Главная</a>
-                <a href="#!">Категории</a>
-                <a href="#!">Профиль</a>
-                <a href="#!">Настройки</a>
+                { links.map(link => (
+                    <a href="#!" key={link.className}>{link.name}</a>
+                )) }
+            </div>
+            <div onClick={() => setShowSidebar(true)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
             </div>
         </div>
+        { showSidebar && <Sidebar close={closeSidebar} links={links} /> }
+        </>
     )
 }
